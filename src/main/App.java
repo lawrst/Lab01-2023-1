92204f88abeb363d914f5971d231774e223f1d70
Lawrence Lopes <81780029+lawrst@users.noreply.github.com>
2023-10-19 20:17:53 +0000

    primeiroteste

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,23 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        for (int i = 0; i < arrayOrdenado.length; i++){
+            if(num == arrayOrdenado[i]){
+                return 1;
+            }
+        }
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int maiorNumero = arrayDesordenado[0];
+        for(int i = 0; i < arrayDesordenado.length; i++){
+            if(arrayDesordenado[i] > maiorNumero){
+                maiorNumero = arrayDesordenado[i];
+            }
+
+        }
+        return maiorNumero;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
